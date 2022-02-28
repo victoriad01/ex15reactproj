b0803a0451d694ae1784ced69ab00474bc2d762a
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2022-02-28 20:22:51 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, sint
+        debitis. Nihil molestias excepturi officiis nobis, illum quasi nemo
+        fugiat cupiditate modi ullam sed explicabo reiciendis dolore veritatis
+        voluptatum eaque expedita similique cumque quod! Quia.
+      </p>
+    </section>
+  )
+}
+
+export default About
